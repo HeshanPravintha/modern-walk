53587327ec08209dd82074773fd655d2c0689c84
HeshanPravintha <31812933+HeshanPravintha@users.noreply.github.com>
2023-05-20 02:27:58 +0530

    Styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,6 +4,7 @@ const NavBar = () => {
   return (
     <div className='navbar'>
         <h1>Modern Walk</h1>
+        <div style={{ borderTop: "4px solid #d9d8d4 "}}></div>
     </div>
   )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,16 @@
-import React from 'react'
-import NavBar from '../components/NavBar'
+import React from "react";
+import NavBar from "../components/NavBar";
 
 function Home() {
   return (
-    
-    <NavBar/>
-    
-  )
+    <>
+      <NavBar />
+      <div className="maindiv">
+        <h3>Flash Sale</h3>
+        <h3>Categories</h3>
+      </div>
+    </>
+  );
 }
 
-export default Home
+export default Home;
