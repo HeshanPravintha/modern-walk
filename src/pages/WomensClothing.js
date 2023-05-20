e3d2668a3f57e6a38bf7423c461cb6c3c67e6e0f
HeshanPravintha <31812933+HeshanPravintha@users.noreply.github.com>
2023-05-20 16:01:51 +0530

    Successfully Create WomensClothing.js Page.

--- src/pages/WomensClothing.js
+++ src/pages/WomensClothing.js
@@ -1,9 +1,73 @@
 import React from 'react'
 import NavBar from '../components/NavBar'
+import ClotheCard from '../components/ClotheCard'
 
 function WomensClothing() {
   return (
-    <NavBar/>
+    <>
+    <NavBar />
+    <div className="maindiv">
+      <h3>Women's Clothing</h3>
+      <div className="womens-clothing-card-row">
+      <ClotheCard 
+        title='Mens Cotton Jacket'
+        imageUrl='https://img.freepik.com/free-photo/blue-black-muscle-car-with-license-plate-that-says-trans-front_1340-23399.jpg'
+        color="secondary-color"
+        price='22'
+        description='Great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as working, hiking...'
+        />
+        <ClotheCard 
+        title='Mens Cotton Jacket'
+        imageUrl='https://img.freepik.com/free-photo/blue-black-muscle-car-with-license-plate-that-says-trans-front_1340-23399.jpg'
+        color="secondary-color"
+        price='22'
+        description='Great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as working, hiking...'
+        />
+        <ClotheCard 
+        title='Mens Cotton Jacket'
+        imageUrl='https://img.freepik.com/free-photo/blue-black-muscle-car-with-license-plate-that-says-trans-front_1340-23399.jpg'
+        color="secondary-color"
+        price='22'
+        description='Great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as working, hiking...'
+        />
+        <ClotheCard 
+        title='Mens Cotton Jacket'
+        imageUrl='https://img.freepik.com/free-photo/blue-black-muscle-car-with-license-plate-that-says-trans-front_1340-23399.jpg'
+        color="secondary-color"
+        price='22'
+        description='Great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as working, hiking...'
+        />
+        <ClotheCard 
+        title='Mens Cotton Jacket'
+        imageUrl='https://img.freepik.com/free-photo/blue-black-muscle-car-with-license-plate-that-says-trans-front_1340-23399.jpg'
+        color="secondary-color"
+        price='22'
+        description='Great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as working, hiking...'
+        />
+        <ClotheCard 
+        title='Mens Cotton Jacket'
+        imageUrl='https://img.freepik.com/free-photo/blue-black-muscle-car-with-license-plate-that-says-trans-front_1340-23399.jpg'
+        color="secondary-color"
+        price='22'
+        description='Great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as working, hiking...'
+        />
+        <ClotheCard 
+        title='Mens Cotton Jacket'
+        imageUrl='https://img.freepik.com/free-photo/blue-black-muscle-car-with-license-plate-that-says-trans-front_1340-23399.jpg'
+        color="secondary-color"
+        price='22'
+        description='Great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as working, hiking...'
+        />
+        <ClotheCard 
+        title='Mens Cotton Jacket'
+        imageUrl='https://img.freepik.com/free-photo/blue-black-muscle-car-with-license-plate-that-says-trans-front_1340-23399.jpg'
+        color="secondary-color"
+        price='22'
+        description='Great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as working, hiking...'
+        />
+      </div>
+    </div>
+  </>
   )
 }
 
